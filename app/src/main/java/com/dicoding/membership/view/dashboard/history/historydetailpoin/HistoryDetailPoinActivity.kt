9189fee503dce9c7ee5c98b5150830c98f648ce9
braceskabane <allzoldyck31@gmail.com>
2024-08-14 15:20:01 +0700

    Test

--- app/src/main/java/com/dicoding/membership/view/dashboard/history/historydetailpoin/HistoryDetailPoinActivity.kt
+++ app/src/main/java/com/dicoding/membership/view/dashboard/history/historydetailpoin/HistoryDetailPoinActivity.kt
@@ -3,8 +3,6 @@ package com.dicoding.membership.view.dashboard.history.historydetailpoin
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.dicoding.membership.R
 
 class HistoryDetailPoinActivity : AppCompatActivity() {
--- app/src/main/java/com/dicoding/membership/view/dashboard/history/historydetailpromo/HistoryDetailPromoActivity.kt
+++ app/src/main/java/com/dicoding/membership/view/dashboard/history/historydetailpromo/HistoryDetailPromoActivity.kt
@@ -3,8 +3,6 @@ package com.dicoding.membership.view.dashboard.history.historydetailpromo
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.dicoding.membership.R
 
 class HistoryDetailPromoActivity : AppCompatActivity() {
