61fa39ba14217d525f700ef469ecc3df7eaef7a5
braceskabane <allzoldyck31@gmail.com>
2024-08-14 15:18:49 +0700

    Test

--- app/src/main/java/com/dicoding/membership/view/dashboard/history/historydetailpoin/HistoryDetailPoinActivity.kt
+++ app/src/main/java/com/dicoding/membership/view/dashboard/history/historydetailpoin/HistoryDetailPoinActivity.kt
@@ -12,10 +12,5 @@ class HistoryDetailPoinActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_history_detail_poin)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
--- app/src/main/java/com/dicoding/membership/view/dashboard/history/historydetailpromo/HistoryDetailPromoActivity.kt
+++ app/src/main/java/com/dicoding/membership/view/dashboard/history/historydetailpromo/HistoryDetailPromoActivity.kt
@@ -12,10 +12,5 @@ class HistoryDetailPromoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_history_detail_promo)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
