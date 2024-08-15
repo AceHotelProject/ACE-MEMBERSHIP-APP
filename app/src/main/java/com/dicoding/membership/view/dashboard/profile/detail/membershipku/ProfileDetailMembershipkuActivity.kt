14c05294ef95a5874c9ef401f5e2d9758aebfc21
Setya <setyahandika70@gmail.com>
2024-08-15 19:45:59 +0700

    fixed

--- app/src/main/java/com/dicoding/membership/view/dashboard/profile/detail/membershipku/ProfileDetailMembershipkuActivity.kt
+++ app/src/main/java/com/dicoding/membership/view/dashboard/profile/detail/membershipku/ProfileDetailMembershipkuActivity.kt
@@ -12,10 +12,5 @@ class ProfileDetailMembershipkuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile_detail_membershipku)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
--- app/src/main/java/com/dicoding/membership/view/dashboard/profile/detail/referralku/ProfileDetailReferralkuActivity.kt
+++ app/src/main/java/com/dicoding/membership/view/dashboard/profile/detail/referralku/ProfileDetailReferralkuActivity.kt
@@ -12,10 +12,5 @@ class ProfileDetailReferralkuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile_detail_referralku)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
