3662582e4aa0a65e3a58f958182063fe7f31418e
Setya <setyahandika70@gmail.com>
2024-08-15 19:43:38 +0700

    add progress transfer dan terima poin

---
+++ app/src/main/java/com/dicoding/membership/view/dashboard/profile/detail/membershipku/ProfileDetailMembershipkuActivity.kt
@@ -0,0 +1,21 @@
+package com.dicoding.membership.view.dashboard.profile.detail.membershipku
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dicoding.membership.R
+
+class ProfileDetailMembershipkuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_profile_detail_membershipku)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/dicoding/membership/view/dashboard/profile/detail/poinku/terima/status/TerimaPoinStatusFragment.kt
+++ app/src/main/java/com/dicoding/membership/view/dashboard/profile/detail/poinku/terima/status/TerimaPoinStatusFragment.kt
@@ -1,4 +1,4 @@
-package com.dicoding.membership.view.dashboard.profile.detail.membershipku
+package com.dicoding.membership.view.dashboard.profile.detail.poinku.terima.status
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ProfileDetailMembershipkuFragment.newInstance] factory method to
+ * Use the [TerimaPoinStatusFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ProfileDetailMembershipkuFragment : Fragment() {
+class TerimaPoinStatusFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class ProfileDetailMembershipkuFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_detail_membershipku, container, false)
+        return inflater.inflate(R.layout.fragment_terima_poin_status, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class ProfileDetailMembershipkuFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileDetailMembershipkuFragment.
+         * @return A new instance of fragment TerimaPoinStatusFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ProfileDetailMembershipkuFragment().apply {
+            TerimaPoinStatusFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
