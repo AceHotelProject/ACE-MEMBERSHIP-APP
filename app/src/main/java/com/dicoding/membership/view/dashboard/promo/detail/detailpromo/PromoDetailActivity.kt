f58acd1d8dc5bcc7928c5a2fb32e71262158fe3d
braceskabane <allzoldyck31@gmail.com>
2024-11-27 20:22:15 +0700

    Revisi v4 + Admin Done

--- app/src/main/java/com/dicoding/membership/view/dashboard/promo/detail/detailpromo/PromoDetailActivity.kt
+++ app/src/main/java/com/dicoding/membership/view/dashboard/promo/detail/detailpromo/PromoDetailActivity.kt
@@ -1,10 +1,8 @@
-package com.dicoding.membership.view.dashboard.promo.detail.detail
+package com.dicoding.membership.view.dashboard.promo.detail.detailpromo
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.dicoding.membership.R
 
 class PromoDetailActivity : AppCompatActivity() {
